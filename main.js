25becca73540274630cd21fa9dcea01e9c14963d
Malinda Mosholder <m.m.mosholder@gmail.com>
2015-04-13 16:06:55 -0600

    part 1 completed

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+
 $(document).on('ready', function() {
-  
+
+  $('.container').on('click', function () {
+  	var mapMarker = $('<div class=map-marker><img src="map-pin.png" /></div>');
+
+	$('.container').append(mapMarker);
+
+	$(mapMarker).css({
+		'top' : event.pageY,
+		'left' : event.pageX 
+	});
+
+  });
+
 });
